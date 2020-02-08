bbbbf76842b23814eab0ff248ea6d43ee52bc6f0
bunkua <fs.golubkov@gmail.com>
2020-02-08 19:16:42 +0200

    step 8 - rev1

---
+++ src/games/progression.php
@@ -0,0 +1,22 @@
+<?php
+
+use function BrainGames\Cli\getPlayerAnswer;
+use function BrainGames\Logic\makeNumber;
+use function BrainGames\Logic\checkAnswer;
+use function BrainGames\Logic\makeProgression;
+
+function playRound()
+{
+    $start = makeNumber(1, 100);
+    $step = makeNumber(2, 20);
+    $progression = makeProgression($start, $step);
+
+    $randomElement = array_rand($progression);
+    $correctAnswer = $progression[$randomElement];
+    $progression[$randomElement] = '..';
+    $question = implode(' ', $progression);
+
+    $answer = (int) getPlayerAnswer($question);
+
+    return checkAnswer($correctAnswer, $answer);
+}
